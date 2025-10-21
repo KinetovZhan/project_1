e9821c0e862328a7ec7d7d9bf7081c5e1b3e5b11
Ваше Имя <khasenabdrakhmanov@gmail.com>
2025-10-21 20:08:53 +0300

    2page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Image from './img/Image.png'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+ // Состояние для активной кнопки
+  const [activeButton, setActiveButton] = useState(null);
+
+  // Функция для обработки нажатия
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+
+  return(
+    <>
+      <header>
+        <div className='mainText'>
+          <h3>Сервис просмотра версий ПО</h3>
+        </div>
+        <div className='navigation'>
+          <h3>Помощь</h3>
+          <h3>Выйти</h3>
+        </div>
       </header>
-    </div>
-  );
+
+
+
+      <main>
+        <div className='table'>
+
+          <div className='sidebar'>
+            <div className='choose'>
+              <button 
+                className={activeButton === 'tractor' ? 'active' : ''}
+                onClick={() => handleButtonClick('tractor')}
+              >
+                Трактор
+                </button>
+                <br />
+              <button
+                className={activeButton === 'aggregates' ? 'active' : ''}
+                onClick={() => handleButtonClick('aggregates')}
+              >
+                Агрегаты
+                </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  ) 
 }
 
-export default App;
+export default App
