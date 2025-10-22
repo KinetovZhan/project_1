fcb0149056ae50292d56d3391664a338f5213e46
krymkovdv <s89232844224@gmail.com>
2025-10-22 14:55:26 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Header, Sidebar, MainPart } from './Func' // ДОБАВИТЬ этот импорт
+import { Header, Sidebar, MainPart } from '../Func' // ДОБАВИТЬ этот импорт
 
 
 function App() {
