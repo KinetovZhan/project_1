93756e4236fdba0bb5d9c3c6457ff32eed7a7c97
Totoyo1 <khasenabdrakhmanov@gmail.com>
2026-01-27 13:20:51 +0300

    YOUYOU

--- src/Function/Filters_tractors.jsx
+++ src/Function/Filters_tractors.jsx
@@ -17,9 +17,9 @@ export function Filters2({ onFilterChangeTracByModel, onFilterChangeByStatus, ac
 
 
   const FilterToTractor = {
-    'К-742МСТ': 'К-742МСТ',
+    'К-742МСТ': 'K-742MCT',
     'К-7': 'K-7', 
-    'К-525': 'К-525'
+    'К-525': 'K-525'
   };
 
   const [FilterTractor_by_status, setFilterTractor_by_status] = useState({
