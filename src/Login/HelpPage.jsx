5a04189e8344a05f997e36e0ed361214853569e5
Totoyo1 <khasenabdrakhmanov@gmail.com>
2026-02-07 17:04:55 +0300

    different_image

--- src/Login/HelpPage.jsx
+++ src/Login/HelpPage.jsx
@@ -1,4 +1,5 @@
 import {useNavigate} from 'react-router-dom';
+import HelpImage from '../img/помощь.jpg';
 export function HelpPage () {
    const navigate = useNavigate();
    const handleBack = () => {
@@ -6,10 +7,12 @@ export function HelpPage () {
    }
    return <div className = "formHelp">
         <button onClick={handleBack} className="add-po-back-button">
-        <svg width="28" height="24" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 22L2 12L12 2M26 22L16 12L26 2" stroke="#1E1E1E" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
-      </button>
-      <h3 className = "textHelp">Господи да поможи</h3>
+          <svg width="28" height="24" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 22L2 12L12 2M26 22L16 12L26 2" stroke="#1E1E1E" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </button>
+        <h3 className = "textHelp">Господи да поможи</h3>
+        <img src={HelpImage} />
+      
       </div>; 
 }
