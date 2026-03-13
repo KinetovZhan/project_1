bd1a1b3c2f3e973f2e3f52cb10400d9a3acffb61
Todrow <pinkol2005@gmail.com>
2026-03-13 10:49:33 +0300

    Исправил адрес загрузки файлов

--- src/Po/Objects.jsx
+++ src/Po/Objects.jsx
@@ -165,7 +165,7 @@ export function Objects({ activeFilters, activeFilters2, selectedModel, selected
   try {
     setDownloading(item.id_Firmwares);
     
-    const response = await fetch(`http://172.20.46.61:8000/software/download/${item.id_Firmwares}`, {
+    const response = await fetch(`api/software/download/${item.id_Firmwares}`, {
       headers: {
         'Authorization': `Bearer ${token}`
       }
