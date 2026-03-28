2da2902ccabd1a580ac88382d6aeb820a4fc7735
krymkovdv <s89232844224@gmail.com>
2026-03-28 17:44:39 +0300

    287.-3.26

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -29,7 +29,7 @@ export function Header({ onLogout, onHelp, onKnowledgeBase, isMobileSidebarOpen,
     const fetchUnread = async () => {
       try{
         if(isModerator){
-          let data = await api.get('/support/unread')
+          let data = await api.get('/support/support/unread-count')
           console.log(data)
           if (data.unread_count !== 0 ){
             setHasUnreadMessages(true)
